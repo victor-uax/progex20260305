c1f3094cb78a8a8e8a4d7c4a882d7a0c36c76d3b
Victor Segura <vseguranes@gmail.com>
2026-03-05 08:57:53 +0100

    chore(CentroFP): creación de la clase CentroFP feat(CentroFP): método constructor

---
+++ src/CentroFP.java
@@ -0,0 +1,16 @@
+public class CentroFP {
+
+    // atributos
+    private Alumno[] alumnos;
+    private final int MAX_ALUMNOS;
+
+    /**
+     * Constructor de la clase CentroFP
+     * @param cant_max_alumnos número máximo que se le asignará a MAX_ALUMNOS recibido como parámetro
+     */
+    public CentroFP(int cant_max_alumnos) {
+        MAX_ALUMNOS = cant_max_alumnos;
+    }
+
+
+}
